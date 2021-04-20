a6bc3b07813877b381745d6f615f35143b511451
fahmi0604 <fahmifirmansyah917@gmail.com>
2021-04-20 22:01:07 +0700

    done

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBKYbZIlps-OEdrTdZd6J-acRyDnfzbFtY",
+    authDomain: "minggu10-7f415.firebaseapp.com",
+    projectId: "minggu10-7f415",
+    storageBucket: "minggu10-7f415.appspot.com",
+    messagingSenderId: "1052999733869",
+    appId: "1:1052999733869:web:afd2224f3b47efdacea564",
+    measurementId: "G-KV4TVRB4JB"
+  };
